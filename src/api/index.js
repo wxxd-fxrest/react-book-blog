83ae73f7e50977acb8ca0750f5e5b065bfcff30d
wxxd-fxrest <fxrest704@gmail.com>
2023-06-06 16:46:15 +0900

    [nodemon] app crashed - waiting for file changes before starting... << 해당 오류 해결 X, 다시 재시작

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,10 @@
+// import Router from 'koa-router';
+// import posts from './posts';
+
 const Router = require('koa-router');
 const posts = require('./posts');
+
+
 const api = new Router();
 
 api.use('/posts', posts.routes()); 
